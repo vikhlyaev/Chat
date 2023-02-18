d7e1ebd25de5a76f872c9f1eacbd597fb9309d2e
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-02-18 14:48:30 +0500

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Anton Vikhlyaev on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
