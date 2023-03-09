d66f45917228b5bccb6a51b37e5fce360ddc7065
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-09 22:25:13 +0500

    Adds day sorted extensions

---
+++ Chat/Extensions/Array + Extensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension Array where Element: DayCategorizable {
+    var daySorted: [Date: [Element]] {
+        var result: [Date: [Element]] = [:]
+        let calendar = Calendar.current
+        self.forEach { item in
+            let i = calendar.startOfDay(for: item.date)
+            if result.keys.contains(i) {
+                result[i]?.append(item)
+            } else {
+                result[i] = [item]
+            }
+        }
+        return result
+    }
+}
