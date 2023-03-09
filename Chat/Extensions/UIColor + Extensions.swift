c5720759a436702f0e8f01d867cf2e9527a7c28a
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-09 08:07:10 +0500

    Adds custom colors in UIColor extension

---
+++ Chat/Extensions/UIColor + Extensions.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIColor {
+    static let customLightGrey = UIColor(red: 0.922, green: 0.922, blue: 0.961, alpha: 0.6)
+    static let customDarkGrey = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+    static let customGreen = UIColor(red: 0.188, green: 0.82, blue: 0.345, alpha: 1)
+    static let customBubbleSent = UIColor(red: 0.27, green: 0.54, blue: 0.97, alpha: 1.00)
+    static let customBubbleReceived = UIColor(red: 0.91, green: 0.91, blue: 0.92, alpha: 1.00)
+    static let customBackground = UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
+}
