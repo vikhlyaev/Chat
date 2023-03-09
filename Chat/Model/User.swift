afcab2c6a31b7eb86ff897e2d2eabd1b94f9e23a
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-09 09:04:49 +0500

    Changes init

--- Chat/Model/User.swift
+++ Chat/Model/User.swift
@@ -3,21 +3,16 @@ import UIKit
 final class User {
     let name: String
     var information: String?
-    var photo: UIImage?
+    var photo: UIImage
     var isOnline: Bool = false
     var hasUnreadMessages: Bool = false
     var messages: [Message]?
     
-    init(name: String, information: String? = nil, withPhotoString photoName: String? = nil) {
+    // временная реализация со строкой для моков
+    init(name: String, information: String? = nil, withPhotoString photoName: String) {
         self.name = name
         self.information = information ?? nil
-        // временная реализация со строкой для моков
-        if let photoName = photoName {
-            guard let photo = UIImage(named: photoName) else { return }
-            self.photo = photo
-        } else {
-            self.photo = UIImage.makeRandomAvatar(with: name)
-        }
+        self.photo = UIImage(named: photoName) ?? UIImage.makeRandomAvatar(with: name)
     }
     
     init(name: String, information: String? = nil, withPhoto photo: UIImage? = nil) {
