568c7df811b1935e79963db78a5d8819528b1035
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-09 11:38:39 +0500

    Adds init to generate a date from a string

--- Chat/Extensions/Date + Extensions.swift
+++ Chat/Extensions/Date + Extensions.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 extension Date {
+    
+    init(_ dateString: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
+        dateFormatter.locale = NSLocale(localeIdentifier: "ru_RU") as Locale
+        let date = dateFormatter.date(from: dateString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+
     func toString() -> String {
         let dateFormatter = DateFormatter()
         if Calendar.current.isDateInToday(self) {
@@ -8,7 +17,6 @@ extension Date {
         } else {
             dateFormatter.dateFormat = "dd MMMM"
         }
-        
         return dateFormatter.string(from: self)
     }
     
