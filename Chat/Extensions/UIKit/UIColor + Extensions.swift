f17af57040151b18b5e3a66491de8b35c6e864d4
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-15 16:20:50 +0500

    Adds new colors

--- Chat/Extensions/UIKit/UIColor + Extensions.swift
+++ Chat/Extensions/UIKit/UIColor + Extensions.swift
@@ -3,8 +3,19 @@ import UIKit
 extension UIColor {
     static let customLightGrey = UIColor(red: 0.922, green: 0.922, blue: 0.961, alpha: 0.6)
     static let customDarkGrey = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
-    static let customGreen = UIColor(red: 0.188, green: 0.82, blue: 0.345, alpha: 1)
-    static let customBubbleSent = UIColor(red: 0.27, green: 0.54, blue: 0.97, alpha: 1.00)
-    static let customBubbleReceived = UIColor(red: 0.91, green: 0.91, blue: 0.92, alpha: 1.00)
-    static let customBackground = UIColor(red: 0.937, green: 0.937, blue: 0.937, alpha: 1)
+    
+    // Common colors
+    static let appGreen = UIColor(red: 0.188, green: 0.82, blue: 0.345, alpha: 1)
+    static let appBubbleSent = UIColor(red: 0.27, green: 0.54, blue: 0.97, alpha: 1.00)
+    static let appBubbleReceived = UIColor(red: 0.91, green: 0.91, blue: 0.92, alpha: 1.00)
+    
+    // Day theme
+    static let dayBackgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    static let daySecondaryBackgroundColor = UIColor(red: 0.949, green: 0.949, blue: 0.969, alpha: 1.0)
+    static let dayLabelColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+    
+    // Night theme
+    static let nightBackgroundColor = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha: 1.0)
+    static let nightSecondaryBackgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.118, alpha: 1.0)
+    static let nightLabelColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 }
