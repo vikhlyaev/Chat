b5cccdc4b79a6b0e1b3e0172c63ba2d7fc6fa5bc
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-16 01:46:49 +0500

    Updates Theme model

--- Chat/Model/Themes/Theme.swift
+++ Chat/Model/Themes/Theme.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-enum Theme: ThemeProtocol {
+enum Theme: Int, ThemeProtocol {
     case day
     case night
     
@@ -12,14 +12,6 @@ enum Theme: ThemeProtocol {
             return "Night"
         }
     }
-    
-    // Colors scheme
-    
-    static let nightBackgroundColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.00)
-    static let nightLightBackgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.12, alpha: 1.00)
-    static let nightLabelColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    static let nightTintColor = UIColor(red: 0.00, green: 0.54, blue: 0.48, alpha: 1.00)
-    
     
     var backgroundColor: UIColor {
         switch self {
