1883b6e9559c0fa5b586e40af547b4e74ccb8eb3
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-23 23:58:32 +0500

    Refactoring code

--- Chat/Services/ThemesManager.swift
+++ Chat/Services/ThemesManager.swift
@@ -1,11 +1,17 @@
 import UIKit
 
+protocol ThemesManagerProtocol: AnyObject {
+    func apply(theme: UIUserInterfaceStyle, completion: @escaping (UIUserInterfaceStyle) -> Void)
+}
+
 final class ThemesManager {
     private let key = "CurrentTheme"
     var currentTheme: UIUserInterfaceStyle {
         UIUserInterfaceStyle(rawValue: UserDefaults.standard.integer(forKey: key)) ?? .light
     }
 }
+
+// MARK: - ThemesManagerProtocol
 
 extension ThemesManager: ThemesManagerProtocol {        
     func apply(theme: UIUserInterfaceStyle, completion: @escaping (UIUserInterfaceStyle) -> Void) {
