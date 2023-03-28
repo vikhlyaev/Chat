daabbbe452d2d48af874f3319bf075a5edf02597
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-28 16:51:08 +0500

    Adds subscript

--- Chat/Model/Configuration/ProfileViewModel.swift
+++ Chat/Model/Configuration/ProfileViewModel.swift
@@ -4,4 +4,12 @@ struct ProfileViewModel {
     var name: String?
     var information: String?
     var photo: UIImage?
+    
+    subscript(index: Int) -> String? {
+        switch index {
+        case 0: return name
+        case 1: return information
+        default: return nil
+        }
+    }
 }
