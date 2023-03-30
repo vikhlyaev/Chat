a8b23c4ce970b4e3fa8b2f6c6199e1737e61102b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-03-30 17:09:44 +0500

    Implements protocol Codable

--- Chat/Model/Configuration/ProfileViewModel.swift
+++ Chat/Model/Configuration/ProfileViewModel.swift
@@ -1,9 +1,14 @@
 import UIKit
 
-struct ProfileViewModel: Equatable {
-    var name: String?
-    var information: String?
+struct ProfileViewModel: Codable {
+    var name = ""
+    var information = ""
     var photo: UIImage?
+    
+    private enum CodingKeys: String, CodingKey {
+       case name
+       case information
+   }
     
     subscript(index: Int) -> String? {
         switch index {
@@ -11,9 +16,5 @@ struct ProfileViewModel: Equatable {
         case 1: return information
         default: return nil
         }
-    }
-    
-    static func ==(lhs: ProfileViewModel, rhs: ProfileViewModel) -> Bool {
-        lhs.name == rhs.name && lhs.information == rhs.information && lhs.photo == rhs.photo
     }
 }
