e5c5c47574725c2526c95b7b9ec6ab1f9d896c94
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-04 19:35:41 +0500

    Changes ConversationsListCellModel

--- Chat/Model/Configuration/ConversationsListCellModel.swift
+++ Chat/Model/Configuration/ConversationsListCellModel.swift
@@ -2,9 +2,7 @@ import UIKit
 
 struct ConversationsListCellModel {
     let name: String
-    var message: String?
-    var date: Date?
-    let isOnline: Bool
-    let hasUnreadMessages: Bool
-    var photo: UIImage
+    let logoURL: String?
+    let lastMessage: String?
+    let lastActivity: Date?
 }
