a6e0e5aba5a31ebfd24a521395feace5ba69a313
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-04 11:48:13 +0500

    Deletes unnecessary code

--- Chat/Screens/ConversationsList/ConversationsListViewController.swift
+++ Chat/Screens/ConversationsList/ConversationsListViewController.swift
@@ -28,33 +28,13 @@ final class ConversationsListViewController: UIViewController {
         }
     }
     
-    private lazy var settingsButton = UIBarButtonItem(image: UIImage(systemName: "gear"),
-                                                      style: .plain,
-                                                      target: self,
-                                                      action: #selector(settingsButtonTapped))
-    
-    private lazy var profileViewButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setBackgroundImage(UIImage(named: "PlaceholderAvatar"), for: .normal)
-        button.addTarget(self, action: #selector(profileButtonTapped), for: .touchUpInside)
-        button.layer.cornerRadius = 16
-        button.clipsToBounds = true
-        return button
-    }()
-    
-    private lazy var profileButton: UIBarButtonItem = {
-        let barButton = UIBarButtonItem()
-        barButton.customView = profileViewButton
-        return barButton
-    }()
-    
     private lazy var conversationsTableView: UITableView = {
         let tableView = UITableView(frame: .zero)
         if #available(iOS 15.0, *) {
             tableView.sectionHeaderTopPadding = 0
         }
-        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 24))
-        tableView.separatorStyle = .none
+        tableView.tableHeaderView = UIView()
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 73, bottom: 0, right: 0)
         tableView.register(ConversationsListCell.self, forCellReuseIdentifier: ConversationsListCell.identifier)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
@@ -80,21 +60,19 @@ final class ConversationsListViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .systemBackground
         view.addSubview(conversationsTableView)
-        
-         cancellable = concurrencyService.photoSubject
-            .sink { _ in
-                print("photo loaded")
-            } receiveValue: { [weak self] image in
-                self?.profileViewButton.setBackgroundImage(image, for: .normal)
-                print("receive image")
-            }
     }
     
     private func setupNavBar() {
-        navigationItem.leftBarButtonItem = settingsButton
-        navigationItem.rightBarButtonItem = profileButton
+        let addChannelsButton = UIBarButtonItem(title: "Add Channel",
+                                                style: .plain,
+                                                target: self,
+                                                action: #selector(addChannelTapped))
+        
+        navigationItem.rightBarButtonItem = addChannelsButton
+        navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.prefersLargeTitles = true
-        title = "Chat"
+        
+        title = "Channels"
     }
     
     private func setDelegates() {
@@ -111,14 +89,8 @@ final class ConversationsListViewController: UIViewController {
                                           photo: user.photo)
     }
     
-    @objc private func settingsButtonTapped() {
-        let settingViewController = SettingsViewController()
-        navigationController?.pushViewController(settingViewController, animated: true)
-    }
-    
-    @objc private func profileButtonTapped() {
-        let profileViewController = ProfileViewController()
-        navigationController?.present(UINavigationController(rootViewController: profileViewController), animated: true)
+    @objc private func addChannelTapped() {
+        print(#function)
     }
 }
 
@@ -132,26 +104,14 @@ extension ConversationsListViewController: UITableViewDelegate {
         navigationController?.pushViewController(conversationViewController, animated: true)
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        ConversationsListHeader(title: TableViewSection.allCases[section].title)
-    }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 24))
-    }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        70
+        76
     }
 }
 
 // MARK: - UITableViewDataSource
 
 extension ConversationsListViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        TableViewSection.allCases.count
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         TableViewSection.allCases[section].array.count
     }
@@ -159,19 +119,17 @@ extension ConversationsListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ConversationsListCell.identifier,
                                                        for: indexPath) as? ConversationsListCell else { return UITableViewCell() }
+        
         let indexLastCellInSection = TableViewSection.allCases[indexPath.section].array.count - 1
         if indexPath.row == indexLastCellInSection {
-            cell.customSeparator.isHidden = true
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
         }
+        
         let currentUser = TableViewSection.allCases[indexPath.section].array[indexPath.row]
         let model = convert(user: currentUser)
         cell.resetCell()
         cell.configure(with: model)
         return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        TableViewSection.allCases[section].title
     }
 }
 
@@ -180,9 +138,6 @@ extension ConversationsListViewController: UITableViewDataSource {
 extension ConversationsListViewController {
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            profileViewButton.widthAnchor.constraint(equalToConstant: 32),
-            profileViewButton.heightAnchor.constraint(equalToConstant: 32),
-            
             conversationsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             conversationsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             conversationsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
