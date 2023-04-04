730d5286587ff837725e828ab4ec8f1f93ac9d8b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-04 18:41:25 +0500

    Updates extensions

--- Chat/Extensions/UIKit/UIImageView + Extensions.swift
+++ Chat/Extensions/UIKit/UIImageView + Extensions.swift
@@ -1,11 +1,10 @@
 import UIKit
 
 extension UIImageView {
-    func load(url: String) {
-        guard let url = URL(string: url) else { return }
+    func load(url: String?) {
+        guard let urlString = url, let url = URL(string: urlString) else { return }
         DispatchQueue.global().async {
-            guard let data = try? Data(contentsOf: url) else { return }
-            guard let image = UIImage(data: data) else { return }
+            guard let data = try? Data(contentsOf: url), let image = UIImage(data: data) else { return }
             DispatchQueue.main.async { self.image = image }
         }
     }
