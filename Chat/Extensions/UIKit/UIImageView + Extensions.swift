22fcbccb154b83d6b87118599cb3ddda98c2a6e3
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-07 00:19:54 +0500

    Removes placeholder channel

--- Chat/Extensions/UIKit/UIImageView + Extensions.swift
+++ Chat/Extensions/UIKit/UIImageView + Extensions.swift
@@ -4,12 +4,8 @@ extension UIImageView {
     func load(url: String?) {
         guard let urlString = url, let url = URL(string: urlString) else { return }
         DispatchQueue.global().async {
-            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
-                DispatchQueue.main.async { self.image = image }
-            } else {
-                DispatchQueue.main.async { self.image = UIImage(named: "PlaceholderChannel") }
-            }
-            
+            guard let data = try? Data(contentsOf: url), let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async { self.image = image }
         }
     }
 }
