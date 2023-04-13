2b05304a4cf0ab23eaec7efd8178150661e69265
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-14 00:30:25 +0500

    Adds logger

--- Chat/Services/CoreDataService.swift
+++ Chat/Services/CoreDataService.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreData
+import OSLog
 
 protocol CoreDataServiceProtocol: AnyObject {
     func fetchChannels() throws -> [ChannelManagedObject]
@@ -7,6 +8,14 @@ protocol CoreDataServiceProtocol: AnyObject {
 }
 
 final class CoreDataService {
+    private var logger: Logger {
+        if ProcessInfo.processInfo.environment.keys.contains("COREDATA_LOGGING") {
+            return Logger(subsystem: Bundle.main.bundleIdentifier ?? "", category: "CoreDataLog")
+        } else {
+            return Logger(.disabled)
+        }
+    }
+    
     private lazy var persistantContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Chat")
         container.loadPersistentStores { _, error in
@@ -26,6 +35,7 @@ final class CoreDataService {
 
 extension CoreDataService: CoreDataServiceProtocol {
     func fetchChannels() throws -> [ChannelManagedObject] {
+        defer { logger.info("\(String(describing: CoreDataService.self)): Successfully received") }
         let fetchRequest = ChannelManagedObject.fetchRequest()
         return try viewContext.fetch(fetchRequest)
     }
@@ -38,8 +48,9 @@ extension CoreDataService: CoreDataServiceProtocol {
                 if backgroundContext.hasChanges {
                     try backgroundContext.save()
                 }
+                logger.info("\(String(describing: CoreDataService.self)): Successfully saved")
             } catch {
-                print(error)
+                logger.error("\(String(describing: CoreDataService.self)): Saving error: \(error)")
             }
         }
     }
