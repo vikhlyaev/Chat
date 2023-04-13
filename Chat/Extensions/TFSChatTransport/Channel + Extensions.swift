2191de695b4fc09ecda1c3d3c8ed77b06130bf8f
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-13 16:51:47 +0500

    Removes TFSChanTransport extensions

--- Chat/Extensions/TFSChatTransport/Channel + Extensions.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-import TFSChatTransport
-
-extension Channel: ChannelProtocol {}
--- Chat/Extensions/TFSChatTransport/Message + Extensions.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-import TFSChatTransport
-
-extension Message: DayCategorizable {}
-extension Message: MessageProtocol {}
