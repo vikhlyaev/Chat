940c48c110fd850145e9a132f40a9eac53303a2b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-14 01:47:30 +0500

    Adds logger and deleting channels

--- Chat/Services/DataSource.swift
+++ Chat/Services/DataSource.swift
@@ -1,13 +1,23 @@
 import Foundation
 import TFSChatTransport
+import OSLog
 
 protocol DataSourceProtocol {
-    func getChannels() -> [ChannelModel]
+    func getChannels(completion: () -> Void) -> [ChannelModel]
     func saveChannelModel(with channelModel: ChannelModel)
+    func deleteChannelModel(with channelModel: ChannelModel)
 }
 
 final class DataSource {
     private let coreDataService: CoreDataServiceProtocol = CoreDataService()
+    
+    private var logger: Logger {
+        if ProcessInfo.processInfo.environment.keys.contains("COREDATA_LOGGING") {
+            return Logger(subsystem: Bundle.main.bundleIdentifier ?? "", category: "CoreDataLog")
+        } else {
+            return Logger(.disabled)
+        }
+    }
 }
 
 // MARK: - DataSourceProtocol
@@ -16,7 +26,8 @@ extension DataSource: DataSourceProtocol {
     
     // MARK: - CoreData Service methods
     
-    func getChannels() -> [ChannelModel] {
+    func getChannels(completion: () -> Void) -> [ChannelModel] {
+        defer { completion() }
         do {
             let channelManagedObjects = try coreDataService.fetchChannels()
             let channels: [ChannelModel] = channelManagedObjects.compactMap { channelManagerObject in
@@ -29,6 +40,7 @@ extension DataSource: DataSourceProtocol {
                 let logoURL = channelManagerObject.logoURL
                 let lastMessage = channelManagerObject.lastMessage
                 let lastActivity = channelManagerObject.lastActivity
+                logger.info("\(String(describing: DataSource.self)): Successfully received - Main Thread: \(Thread.isMainThread)")
                 return ChannelModel(id: id,
                                     name: name,
                                     logoURL: logoURL,
@@ -37,10 +49,9 @@ extension DataSource: DataSourceProtocol {
             }
             return channels
         } catch {
-            print(error)
+            logger.error("\(String(describing: DataSource.self)): Error receiving channels - Main Thread: \(Thread.isMainThread) - Error: \(error)")
             return []
         }
-        
     }
     
     func saveChannelModel(with channelModel: ChannelModel) {
@@ -51,6 +62,19 @@ extension DataSource: DataSourceProtocol {
             channelManagedObject.logoURL = channelModel.logoURL
             channelManagedObject.lastMessage = channelModel.lastMessage
             channelManagedObject.lastActivity = channelModel.lastActivity
+        }
+    }
+    
+    func deleteChannelModel(with channelModel: ChannelModel) {
+        coreDataService.save { context in
+            let fetchRequest = ChannelManagedObject.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "id == %@", channelModel.id as CVarArg)
+            do {
+                guard let channelManagedObject = try context.fetch(fetchRequest).first else { return }
+                context.delete(channelManagedObject)
+            } catch {
+                logger.error("\(String(describing: DataSource.self)): Delete channel error - Main Thread: \(Thread.isMainThread) - Error: \(error)")
+            }
         }
     }
 }
