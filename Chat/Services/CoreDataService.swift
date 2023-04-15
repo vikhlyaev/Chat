e23dd6621e9dba35a5074afd38bb83099cd35ce2
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-15 19:43:55 +0500

    Adds fetching messages

--- Chat/Services/CoreDataService.swift
+++ Chat/Services/CoreDataService.swift
@@ -4,6 +4,7 @@ import OSLog
 
 protocol CoreDataServiceProtocol: AnyObject {
     func fetchChannels() throws -> [ChannelManagedObject]
+    func fetchMessages(for channelId: String) throws -> [MessageManagedObject]
     func fetchResultController(entityName: String, sortName: String) -> NSFetchedResultsController<NSFetchRequestResult>
     func save(block: (NSManagedObjectContext) throws -> Void)
 }
@@ -38,19 +39,30 @@ extension CoreDataService: CoreDataServiceProtocol {
     
     func fetchResultController(entityName: String, sortName: String) -> NSFetchedResultsController<NSFetchRequestResult> {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
-        let sortDescriptor = NSSortDescriptor(key: sortName, ascending: true)
+        let sortDescriptor = NSSortDescriptor(key: sortName, ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
-        let fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest,
-                                                               managedObjectContext: viewContext,
-                                                               sectionNameKeyPath: nil,
-                                                               cacheName: nil)
-        return fetchResultController
+        return NSFetchedResultsController(fetchRequest: fetchRequest,
+                                          managedObjectContext: viewContext,
+                                          sectionNameKeyPath: nil,
+                                          cacheName: nil)
     }
     
     func fetchChannels() throws -> [ChannelManagedObject] {
-        defer { logger.info("\(String(describing: CoreDataService.self)): Successfully received") }
         let fetchRequest = ChannelManagedObject.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "lastActivity", ascending: false)]
         return try viewContext.fetch(fetchRequest)
+    }
+    
+    func fetchMessages(for channelId: String) throws -> [MessageManagedObject] {
+        let fetchRequest = ChannelManagedObject.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", channelId as CVarArg)
+        guard
+            let channelManagedObject = try viewContext.fetch(fetchRequest).first,
+            let messagesManagedObjects = channelManagedObject.messages?.array as? [MessageManagedObject]
+        else {
+            return []
+        }
+        return messagesManagedObjects
     }
     
     func save(block: (NSManagedObjectContext) throws -> Void) {
@@ -66,5 +78,13 @@ extension CoreDataService: CoreDataServiceProtocol {
                 logger.error("\(String(describing: CoreDataService.self)): Saving error: \(error)")
             }
         }
+    }
+}
+
+struct Constants {
+    struct CoreData {
+        static let channelEntityName = "ChannelManagedObject"
+        static let messageEntityName = "MessageManagedObject"
+        static let channelSortName = "lastActivity"
     }
 }
