fb88a7e11f4b44021bd60433054aa80a89e3ab87
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-18 18:27:22 +0500

    Removes addMessage method

--- Chat/Model/SortedMessage.swift
+++ Chat/Model/SortedMessage.swift
@@ -3,10 +3,6 @@ import Foundation
 struct SortedMessage {
     let date: Date
     var messages: [MessageModel]
-    
-    mutating func addMessage(_ message: MessageModel) {
-        messages.insert(message, at: 0)
-    }
 }
 
 protocol DayCategorizable {
