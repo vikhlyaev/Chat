af350fcfbe10de6dbdda8b0c3b8237259318700d
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-29 00:16:54 +0500

    Temporary adds service assembly singletone

---
+++ Chat/Assembly/ServiceAssembly.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+final class ServiceAssembly {
+    
+    static let shared = ServiceAssembly()
+    
+    private let fileManagerService: FileManagerService = FileManagerServiceImpl(
+        logService: LogServiceImpl(name: "FileManagerService")
+    )
+    
+    let coreDataService: CoreDataService = CoreDataServiceImpl(
+        logService: LogServiceImpl(name: "CoreDataService")
+    )
+    
+    private let networkService: NetworkService = NetworkServiceImpl(
+        session: URLSession.shared,
+        logService: LogServiceImpl(name: "NetworkService")
+    )
+    
+    private init() {}
+    
+    func makeThemesService(with logService: LogService = LogServiceImpl(name: "ThemesService")) -> ThemesService {
+        ThemesServiceImpl(logService: logService)
+    }
+    
+    func makeProfileService(with logService: LogService = LogServiceImpl(name: "ProfileService")) -> ProfileService {
+        ProfileServiceImpl(coreDataService: coreDataService,
+                           fileManagerService: fileManagerService,
+                           logService: logService)
+    }
+    
+    func makePhotoLoaderService(with logService: LogService = LogServiceImpl(name: "PhotoLoaderService")) -> PhotoLoaderService {
+        PhotoLoaderServiceImpl(networkService: networkService,
+                               logService: logService)
+    }
+}
