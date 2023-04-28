a202da5154f49a03efc86eca7dd1f09b467e89c6
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-28 10:52:28 +0500

    Refactoring code

--- Chat/Core Layer/FileManager/FileManagerService.swift
+++ Chat/Core Layer/FileManager/FileManagerService.swift
@@ -4,4 +4,3 @@ protocol FileManagerService {
     func read(by filename: String, _ completion: @escaping (Result<Data, Error>) -> Void)
     func write(_ data: Data, with filename: String, _ completion: @escaping (Error?) -> Void)
 }
-
