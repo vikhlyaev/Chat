52b90d6bd1358387df3186c833cbc7174d68cd72
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-28 15:49:46 +0500

    Updates profile model

--- Chat/Model/ProfileModel.swift
+++ Chat/Model/ProfileModel.swift
@@ -1,10 +1,15 @@
 import UIKit
 
-struct ProfileModel: Codable {
-    let id = UUID()
+struct ProfileModel {
     var name: String?
     var information: String?
     var photo: UIImage?
+    
+    init(name: String? = nil, information: String? = nil, photo: UIImage? = nil) {
+        self.name = name
+        self.information = information
+        self.photo = photo
+    }
     
     private enum CodingKeys: String, CodingKey {
        case name
