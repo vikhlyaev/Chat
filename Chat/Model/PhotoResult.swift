a4f87a033edc9c8d92a119f9e6a49016f390b079
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-29 00:16:17 +0500

    Fixes model

--- Chat/Model/PhotoResult.swift
+++ Chat/Model/PhotoResult.swift
@@ -2,6 +2,10 @@ import Foundation
 
 struct PhotoResult: Decodable {
     let photos: [Photo]
+    
+    enum CodingKeys: String, CodingKey {
+        case photos = "hits"
+    }
 }
 
 struct Photo: Decodable {
