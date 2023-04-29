fffaf1ce975f7d2d0ea33756a61bbb1f5ef8c9d4
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-04-29 16:51:30 +0500

    Removes didSelectPhoto method

--- Chat/PhotoSelection/View/Custom Views/PhotoSelectionCellDelegate.swift
+++ Chat/PhotoSelection/View/Custom Views/PhotoSelectionCellDelegate.swift
@@ -2,5 +2,4 @@ import UIKit
 
 protocol PhotoSelectionCellDelegate: AnyObject {
     func didRecievePhoto(for photoModel: PhotoModel, _ completion: @escaping (UIImage?) -> Void)
-    func didSelectPhoto()
 }
