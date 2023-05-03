521c1be6f6ef1fe27adb699945f4c4cbd86ac3b7
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-03 20:08:30 +0500

    Implements module output protocol

--- Chat/Coordinators/ChannelsCoordinator.swift
+++ Chat/Coordinators/ChannelsCoordinator.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 final class ChannelsCoordinator {
-    
+
     let navigationController: UINavigationController
     private let moduleAssembly: ModuleAssembly
     
@@ -25,7 +25,15 @@ final class ChannelsCoordinator {
 
 extension ChannelsCoordinator: ChannelsListModuleOutput {
     func moduleWantsToOpenChannel(with channel: ChannelModel) {
-        let vc = moduleAssembly.makeChannelModule(with: channel)
+        let vc = moduleAssembly.makeChannelModule(with: channel, and: self)
         navigationController.pushViewController(vc, animated: true)
+    }
+}
+
+extension ChannelsCoordinator: ChannelModuleOutput {
+    func moduleWantsToOpenPhotoSelection(with delegate: PhotoSelectionDelegate) {
+        let vc = moduleAssembly.makePhotoSelectionModule(with: delegate)
+        let window = UIApplication.shared.windows.filter { $0.isKeyWindow }.first
+        window?.rootViewController?.present(vc, animated: true)
     }
 }
