75f623f7d95891844fdfadcd66db547fb632ff81
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-03 15:14:14 +0500

    Updates network service

--- Chat/Core Layer/Network/NetworkServiceImpl.swift
+++ Chat/Core Layer/Network/NetworkServiceImpl.swift
@@ -3,12 +3,9 @@ import Foundation
 final class NetworkServiceImpl {
     private let session: URLSession = URLSession.shared
     private let fileManagerService: FileManagerService
-    private let logService: LogService
     
-    init(fileManagerService: FileManagerService,
-         logService: LogService) {
+    init(fileManagerService: FileManagerService) {
         self.fileManagerService = fileManagerService
-        self.logService = logService
     }
 }
 
@@ -16,35 +13,32 @@ final class NetworkServiceImpl {
 
 extension NetworkServiceImpl: NetworkService {
     func fetch<T: Decodable>(for request: URLRequest, _ completion: @escaping (Result<T, Error>) -> Void) {
-        session.dataTask(with: request, completionHandler: { [weak self] data, response, error in
+        session.dataTask(with: request, completionHandler: { data, response, error in
             if let data = data, let response = response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
                 if 200 ..< 300 ~= statusCode {
                     do {
                         let object = try JSONDecoder().decode(T.self, from: data)
                         completion(.success(object))
-                        self?.logService.success("Data decoded")
                     } catch {
                         completion(.failure(NetworkError.parsingJsonError(error)))
-                        self?.logService.error("Data not decoded. Error: \(error)")
                     }
                 } else {
                     completion(.failure(NetworkError.httpStatusCode(statusCode)))
-                    self?.logService.error("HTTP Status code: \(statusCode)")
                 }
             } else if let error = error {
                 completion(.failure(NetworkError.urlRequestError(error)))
-                self?.logService.error("URLRequest error: \(error)")
             } else {
                 completion(.failure(NetworkError.urlSessionError))
-                self?.logService.error("URLSession error")
             }
         }).resume()
     }
     
     func download(with request: URLRequest, _ completion: @escaping (Result<Data, Error>) -> Void) {
-        guard let photoName = request.url?.lastPathComponent else { return }
-        if fileManagerService.fileExist(at: photoName) {
-            fileManagerService.read(by: photoName) { result in
+        guard let filename = request.url?.lastPathComponent else {
+            return
+        }
+        if fileManagerService.fileExist(at: filename) {
+            fileManagerService.read(by: filename) { result in
                 switch result {
                 case .success(let cachedPhotoData):
                     completion(.success(cachedPhotoData))
@@ -58,28 +52,41 @@ extension NetworkServiceImpl: NetworkService {
                     if 200 ..< 300 ~= statusCode {
                         do {
                             let data = try Data(contentsOf: location)
-                            self?.fileManagerService.write(data, with: photoName, { error in
+                            self?.fileManagerService.write(data, with: filename) { error in
                                 if let error = error {
                                     completion(.failure(error))
                                 }
-                            })
+                            }
                             completion(.success(data))
                         } catch {
                             completion(.failure(NetworkError.invalidData))
-                            self?.logService.error("Data not decoded. Error: \(error)")
                         }
                     } else {
                         completion(.failure(NetworkError.httpStatusCode(statusCode)))
-                        self?.logService.error("HTTP Status code: \(statusCode)")
                     }
                 } else if let error = error {
                     completion(.failure(NetworkError.urlRequestError(error)))
-                    self?.logService.error("URLRequest error: \(error)")
                 } else {
                     completion(.failure(NetworkError.urlSessionError))
-                    self?.logService.error("URLSession error")
                 }
             }.resume()
         }
+    }
+    
+    func checkImageContentType(with request: URLRequest, _ completion: @escaping (Result<Bool, Error>) -> Void) {
+        session.dataTask(with: request) { _, response, error in
+            if let response = response, let httpUrlResponse = (response as? HTTPURLResponse) {
+                if 200 ..< 300 ~= httpUrlResponse.statusCode {
+                    if let contentType = httpUrlResponse.value(forHTTPHeaderField: "Content-Type") {
+                        let imagesTypes = ["image/jpeg", "image/png", "image/svg+xml"]
+                        completion(.success(imagesTypes.contains(contentType)))
+                    }
+                } else {
+                    completion(.failure(NetworkError.httpStatusCode(httpUrlResponse.statusCode)))
+                }
+            } else if let error = error {
+                completion(.failure(NetworkError.urlRequestError(error)))
+            }
+        }.resume()
     }
 }
