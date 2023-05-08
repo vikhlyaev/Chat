d3202baae3a3941a766bf4d84fb757067c4e9b44
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-08 15:43:03 +0500

    Adds photo adding view

---
+++ Chat/Presentation Layer/Common Views/PhotoAddingView.swift
@@ -0,0 +1,103 @@
+import UIKit
+
+final class PhotoAddingView: UIView {
+    
+    // MARK: - UI
+    
+    private lazy var wrapperView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var addPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add Photo", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17)
+        button.addTarget(
+            self,
+            action: #selector(addPhotoButtonTapped),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let photoWidth: CGFloat
+    private let completion: () -> Void
+    
+    // MARK: - Init
+    
+    init(
+        photoWidth: CGFloat,
+        completion: @escaping () -> Void
+    ) {
+        self.photoWidth = photoWidth
+        self.completion = completion
+        super.init(frame: .zero)
+        
+        setupView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        photoImageView.layer.cornerRadius = photoWidth / 2
+    }
+    
+    // MARK: - Setup UI
+    
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(wrapperView)
+        wrapperView.addSubview(photoImageView)
+        wrapperView.addSubview(addPhotoButton)
+    }
+    
+    // MARK: - Add Button actions
+    
+    @objc
+    private func addPhotoButtonTapped() {
+        completion()
+    }
+    
+    func setPhoto(_ photo: UIImage?) {
+        photoImageView.image = photo
+    }
+}
+
+// MARK: - Settings Constraints
+
+extension PhotoAddingView {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            wrapperView.topAnchor.constraint(equalTo: topAnchor),
+            wrapperView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            wrapperView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            wrapperView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            photoImageView.topAnchor.constraint(equalTo: wrapperView.topAnchor),
+            photoImageView.centerXAnchor.constraint(equalTo: wrapperView.centerXAnchor),
+            photoImageView.widthAnchor.constraint(equalToConstant: photoWidth),
+            photoImageView.heightAnchor.constraint(equalToConstant: photoWidth),
+            
+            addPhotoButton.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 24),
+            addPhotoButton.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor),
+            addPhotoButton.centerXAnchor.constraint(equalTo: wrapperView.centerXAnchor)
+        ])
+    }
+}
