77c186fb0856e8071f81af4f70ea1dbd791b3019
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-15 12:35:40 +0500

    Refactoring code

--- Chat/Service Layer/SSETransportService/SSETransportServiceImpl.swift
+++ Chat/Service Layer/SSETransportService/SSETransportServiceImpl.swift
@@ -5,14 +5,12 @@ import Combine
 final class SSETransportServiceImpl {
     private var sseService: SSEService?
     private var cancellables = Set<AnyCancellable>()
-    
+    private let host = Bundle.main.object(forInfoDictionaryKey: "Chat Service IP") as? String
+    private let port = Bundle.main.object(forInfoDictionaryKey: "Chat Service Port") as? String
 }
 
 extension SSETransportServiceImpl: SSETransportService {
     func subscribeOnEvents() -> AnyPublisher<ChatEvent, Error>? {
-        let host = Bundle.main.object(forInfoDictionaryKey: "Chat Service IP") as? String
-        let port = Bundle.main.object(forInfoDictionaryKey: "Chat Service Port") as? String
-        
         if let host, let port, let portInt = Int(port) {
             sseService = SSEService(
                 host: host,
